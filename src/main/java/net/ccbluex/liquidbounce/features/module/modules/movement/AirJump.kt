66b6336864325d462ea1671c78b3c2c63ba798fb
CMCat-Xicheng <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-08-09 17:08:23 +0800

    fixed airjump issue

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AirJump.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AirJump.kt
@@ -1,8 +1,12 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.minecraft.client.settings
+import net.minecraft.entity.player
 
 @ModuleInfo(name = "AirJump", category = ModuleCategory.MOVEMENT)
 class AirJump : Module() {
