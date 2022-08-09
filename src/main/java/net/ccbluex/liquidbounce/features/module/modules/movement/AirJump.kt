8954ca7d42648a98560c70b4a8ca7ad9b7d0cfc3
CMCat-Xicheng <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-08-09 16:56:30 +0800

    updated airjump

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AirJump.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AirJump.kt
@@ -5,4 +5,9 @@ import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 
 @ModuleInfo(name = "AirJump", category = ModuleCategory.MOVEMENT)
-class AirJump : Module()
+class AirJump : Module() {
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+      if (mc.gameSettings.keyBindJump.pressed && !mc.thePlayer.onGround) mc.thePlayer.jump()
+    }
+}
