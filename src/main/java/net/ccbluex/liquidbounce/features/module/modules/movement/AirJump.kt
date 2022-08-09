f2639f04a75b5961f7e688e02cab53e0a2027a71
CMCat-Xicheng <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-08-09 16:28:30 +0800

    airjump back

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AirJump.kt
@@ -0,0 +1,8 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+
+@ModuleInfo(name = "AirJump", category = ModuleCategory.MOVEMENT)
+class AirJump : Module()
