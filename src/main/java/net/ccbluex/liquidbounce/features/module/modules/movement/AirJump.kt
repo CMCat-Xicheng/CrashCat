dea376414eb7de21f0c92274eb30a9ecbe2f7428
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-08-10 09:37:45 +0800

    airjump,yes!

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AirJump.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AirJump.kt
@@ -1,1 +1,17 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement
 
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.minecraft.client.settings.KeyBinding
+import net.minecraft.entity.player.EntityPlayer
+
+@ModuleInfo(name = "AirJump", category = ModuleCategory.MOVEMENT)
+class AirJump : Module() {
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+      if (mc.gameSettings.keyBindJump.pressed && !mc.thePlayer.onGround) mc.thePlayer.jump()
+    }
+}
