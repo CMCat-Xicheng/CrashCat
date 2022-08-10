b149ec2ce2d78d9090b99f0a3e96251de985d3f5
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-08-10 11:19:06 +0800

    my client

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/ScriptManager.java
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/ScriptManager.java
@@ -10,8 +10,8 @@ import net.ccbluex.liquidbounce.features.module.ModuleCategory;
 import net.ccbluex.liquidbounce.features.module.ModuleInfo;
 import net.ccbluex.liquidbounce.launch.data.modernui.GuiScriptLoadMenu;
 
-@ModuleInfo(name = "FDPScriptManager", category = ModuleCategory.CLIENT)
-public class FDPScriptManager extends Module {
+@ModuleInfo(name = "ScriptManager", category = ModuleCategory.CLIENT)
+public class ScriptManager extends Module {
     @Override
     public void onEnable() {
         mc.displayGuiScreen(new GuiScriptLoadMenu());
