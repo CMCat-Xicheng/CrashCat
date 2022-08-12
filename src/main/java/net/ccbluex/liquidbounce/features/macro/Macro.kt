791ffca85d244424a29806152f1092f49b8fe2da
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-08-12 22:15:07 +0800

    Update Macro.kt

--- src/main/java/net/ccbluex/liquidbounce/features/macro/Macro.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/macro/Macro.kt
@@ -1,6 +1,7 @@
 package net.ccbluex.liquidbounce.features.macro
 
 import net.ccbluex.liquidbounce.LiquidBounce
+import net.minecraft.entity.player.EntityPlayer
 
 class Macro(val key: Int, val command: String) {
     fun exec() {
