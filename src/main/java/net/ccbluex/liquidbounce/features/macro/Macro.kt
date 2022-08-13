5a8145ce5fc2d0e13a45cc1b1bae05b7829cbe18
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-08-13 09:31:08 +0800

    reroll

--- src/main/java/net/ccbluex/liquidbounce/features/macro/Macro.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/macro/Macro.kt
@@ -1,10 +1,9 @@
 package net.ccbluex.liquidbounce.features.macro
 
 import net.ccbluex.liquidbounce.LiquidBounce
-import net.minecraft.entity.player.EntityPlayer
 
 class Macro(val key: Int, val command: String) {
     fun exec() {
-        mc.thePlayer.sendChatMessage(command)
+        LiquidBounce.commandManager.executeCommands(command)
     }
 }
